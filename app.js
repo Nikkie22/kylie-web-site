f100ea08fb5236e73d933ef8445efaaa0f21b5ac
Nikkie22 <nigar010106@gmail.com>
2024-02-20 14:29:36 +0400

    web site coding
    I wrote the code with HTML and CSS. JavaScript has no effect.

---
+++ app.js
@@ -0,0 +1,55 @@
+//Document Write ile cikti verme
+
+// document.write("Nigar Khalilova <br>");
+// document.write("Hello World <br>");
+// document.write(5.5);
+
+//console log metodu kullaniriz
+
+//console.log("Hello World");
+//console.log("Nigar Khalilova");
+//console.log(3.53);
+//console.log(true);
+//console.log({name:"Sheggy" ,surname :"Doo"});
+
+////console.clear();
+
+//let a=5;
+//let b=10;
+//debugger;
+//console.log("Iki sayinin toplami:",a+b);
+//console.clear();
+
+
+// Alert (Uyari) popup ile cikti verme islemleri
+
+//console.log(window);
+
+//let a=5;
+//let b=15;
+
+//alert("Iki sayinin toplami : " + (a+b));
+//document.write("Nigar Khalilova"); 
+
+
+//class MagicParticle {
+    //constructor(x, y) {
+       // this.x = x;
+        //this.y = y;
+        //this.size = random(10, 30);
+        //this.baseColor = color(
+          //  random([
+            //"#e84e66" , "#67c69e",
+            //"#edf1f4" , "#80acc9",
+            //"#73a8b0" , "#fe817f",
+            //"#68d2a4" , "#1d203f",
+            //"#c9a30d" ])
+        //),
+
+       // update() {      
+         //   this.x += this.speedX;
+           // this.y += this.speedY;
+            //this.opacity -= 10;
+        //}
+    //}
+//}
